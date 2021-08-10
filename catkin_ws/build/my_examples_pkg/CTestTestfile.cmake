6108d6ace331ff76d4476b9083eaab911f8d9d82
Xiaolu Wang <38363960+lulu1986@users.noreply.github.com>
2021-08-10 08:51:08 +0200

    Delete catkin_ws directory

--- catkin_ws/build/my_examples_pkg/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pi/catkin_ws/src/my_examples_pkg
-# Build directory: /home/pi/catkin_ws/build/my_examples_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
